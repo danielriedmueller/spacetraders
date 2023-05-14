761d5dd48ecea1bd6e56c61a04f01df9f81dbc9b
danielriedmueller <daniel.riedmuller@gmail.com>
2023-05-14 10:59:46 +0200

    config

--- config/dalle.php
+++ config/dalle.php
@@ -3,5 +3,5 @@
 return [
     'api' => env('DALL_E_API', 'https://api.openai.com/v1/images'),
     'key' => env('DALL_E_KEY', ''),
-    'disabled' => env('DALL_E_DISABLED', false),
+    'disabled' => env('DALL_E_DISABLED', true),
 ];
